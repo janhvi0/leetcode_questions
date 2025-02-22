fed8d1910efa0a4af811654d12ca074c9c101319
Janhvi Patriwar <102748690+janhvi0@users.noreply.github.com>
2025-02-22 23:16:07 +0530

    Time: 61 ms (5.08%) | Memory: 17.7 MB (92.71%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,5 +1,6 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #brute force with more time and space complexity
         arr=set()
         n=len(nums1)
         m=len(nums2)
